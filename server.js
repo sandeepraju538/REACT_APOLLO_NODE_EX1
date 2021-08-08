b07f260821880e8237d1722eff41950314cb35d5
Sandeep Raju K <sandeepraju538@gmail.com>
2021-08-08 11:06:40 +0530

    heroku based configuration changes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require("dotenv").config({ path: "./config.env" });
 const express = require('express');
 const schema = require('./schema');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,5 +14,17 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('App running');
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
